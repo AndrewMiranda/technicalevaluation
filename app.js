7a994038941491bab8bbf4db8339090814c58ed2
Andrew Miranda <tiburonandrew@gmail.com>
2022-08-23 22:28:59 -0500

    Set Up

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const fetch = require("node-fetch");
 const express = require('express');
 const path = require('path');
 const app = express();
 
 // settings
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
